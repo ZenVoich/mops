95ece0f8001b084db5cf05ec908000f55eb3f9e4
Zen Voich <zen.voich@gmail.com>
2022-05-27 17:19:08 +0300

    -

--- frontend/components/SearchResults.svelte
+++ frontend/components/SearchResults.svelte
@@ -61,62 +61,6 @@
 		gap: 20px;
 	}
 
-	.package:hover {
-		box-shadow: -0 1px 5px #c1c1c1;
-	}
-
-	.package {
-		display: flex;
-		padding: 17px;
-		box-sizing: border-box;
-		width: 600px;
-		max-width: 100%;
-		gap: 10px;
-		box-shadow: 0 1px 3px #c1c1c1;
-		border-radius: 3px;
-	}
-
-	.left {
-		flex-grow: 1;
-		display: flex;
-		flex-direction: column;
-		gap: 10px;
-		min-width: 0;
-	}
-
-	.header {
-		display: flex;
-		align-items: flex-end;
-		gap: 10px;
-	}
-
-	.name {
-		font-size: 20px;
-		font-weight: 500;
-		cursor: pointer;
-	}
-
-	.version {
-		font-size: 12px;
-		display: none;
-	}
-
-	.description {
-		font-size: 15px;
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-	}
-
-	.right {
-		display: flex;
-		flex-direction: column;
-		gap: 4px;
-		font-size: 14px;
-		color: rgb(170, 170, 170);
-		white-space: nowrap;
-	}
-
 	.not-found {
 		font-size: 20px;
 		color: #9d9d9d;
