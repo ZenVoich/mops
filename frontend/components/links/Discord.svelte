6ab68a95a0c8897211823243ff6cb4280efd08f7
Zen Voich <zen.voich@gmail.com>
2023-04-07 09:02:03 +0400

    [frontend] add discord link

---
+++ frontend/components/links/Discord.svelte
@@ -0,0 +1,12 @@
+<a aria-label="Mops on Discord" href="https://discord.gg/MfWpnf2X" target="_blank" rel="external noopener norefferer">
+	<img src="/img/discord.svg" alt="Discord logo" loading="lazy" />
+</a>
+
+<style>
+	img {
+		width: 30px;
+		height: 30px;
+		padding: 1.5px;
+		box-sizing: border-box;
+	}
+</style>
