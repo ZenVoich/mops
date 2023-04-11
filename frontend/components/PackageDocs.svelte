1a3316632a68aebe700ed18029cce999093d28cd
Zen Voich <zen.voich@gmail.com>
2023-04-11 10:25:44 +0400

    [frontend] package docs: beautify type definition

--- frontend/components/PackageDocs.svelte
+++ frontend/components/PackageDocs.svelte
@@ -7,10 +7,18 @@
 	import {toHtml} from 'hast-util-to-html';
 	import {onMount} from 'svelte';
 
+	type DefinitionKind = 'module' | 'class' | 'type' | 'func' | 'value' | 'type-actor';
+	type Definition = {
+		id: string;
+		name: string;
+		kind: DefinitionKind;
+	}
+
 	export let docsData: Uint8Array;
+
 	let docsHtml = 'Loading...';
 	let files: any[] = [];
-	let definitions: any[] = [];
+	let definitions: Definition[] = [];
 	let fileNameShadow = false;
 
 	function getDefaultFileName(files: any[]): string {
@@ -28,6 +36,10 @@
 		files = await untar(decompressed.buffer);
 	};
 
+	function getModuleNesting(id: string): number {
+		return [...id.replace('type.', '').matchAll(/\./g)].length;
+	}
+
 	let render = async (files: any[], fileName = getDefaultFileName(files)) => {
 		let file = files.find((file) => file.name.replace('.adoc', '') == fileName);
 		if (!file) {
@@ -36,17 +48,89 @@
 
 		let div = document.createElement('div');
 
+		// adoc to html
 		let text = new TextDecoder().decode(file.buffer);
 		// @ts-ignore
 		let asciidoctor = window.Asciidoctor();
 		let doc = asciidoctor.load(text);
-		definitions = Object.keys(doc.getRefs()).slice(1);
+
+		// definitions
+		let getKind = (line: string): DefinitionKind => {
+			if (line.match(/^\w+$/i)) {
+				return 'module';
+			}
+			if (line.startsWith('class')) {
+				return 'class';
+			}
+			if (line.match(/type \w+ = actor {/)) {
+				return 'type-actor';
+			}
+			if (line.startsWith('type')) {
+				return 'type';
+			}
+			if (line.startsWith('func') || line.includes(' -> ')) {
+				return 'func';
+			}
+			return 'value';
+		};
+		definitions = Object.values(doc.getRefs()).slice(1).map((def: any) => {
+			return {
+				id: def.id,
+				name: def.id.replace('type.', '').split('.').at(-1),
+				kind: def.blocks[0]?.node_name === 'listing' ? getKind(def.blocks[0]?.lines[0]) : 'value',
+			};
+		});
+
 		div.innerHTML = doc.convert();
 
-		// syntax highlight
+		// nested fields
+		div.querySelectorAll('h3').forEach((el) => {
+			if (getModuleNesting(el.id)) {
+				let section = el.closest('.sectionbody') as HTMLElement;
+				if (section) {
+					// let sectionTitle = section.previousElementSibling as HTMLElement;
+					// sectionTitle.previousElementSibling.style.position = 'sticky';
+					// sectionTitle.previousElementSibling.style.top = '72px';
+					// sectionTitle.previousElementSibling.style.background = 'white';
+					// sectionTitle.previousElementSibling.style.borderBottom = '1px solid gray';
+
+					section.style.marginLeft = '0px';
+					section.style.paddingLeft = '20px';
+					// section.style.borderLeft = '2px solid #e2e2e2';
+					// section.style.borderLeft = '2px dotted rgb(222 222 222)';
+					section.style.borderLeft = '1px solid rgb(222 222 222)';
+				}
+			}
+		});
+
 		let starryNight = await createStarryNight([moGrammar]);
 		div.querySelectorAll('pre code.language-motoko, pre code.language-mo').forEach((el) => {
-			el.innerHTML = toHtml(starryNight.highlight(el.textContent, 'source.mo'));
+			let text = el.textContent;
+
+			// beautify
+			let indentLevel = 0;
+			if (el.closest('.no-repl')) {
+				text = text.replaceAll(/(;)\s|(\{)\s|\s(\})/g, '$1$2$3')
+					.replace(/\{|\}|;/g, (m0) => {
+						if (m0 == '{') {
+							indentLevel++;
+						}
+						if (m0 == '}') {
+							indentLevel--;
+						}
+
+						let indent = '\t'.repeat(indentLevel);
+
+						if (m0 == ';' || m0 == '{') {
+							return `${m0}\n${indent}`;
+						}
+						return `\n${indent}${m0}`;
+					})
+					.replaceAll('}\t;', '};');
+			}
+
+			// syntax highlight
+			el.innerHTML = toHtml(starryNight.highlight(text, 'source.mo'));
 		});
 
 		docsHtml = div.innerHTML;
@@ -65,6 +149,7 @@
 	let defsPanelTop = '';
 	let footerHeight = document.querySelector('#app-footer').getBoundingClientRect().height;
 	let margin = 20;
+	let bottomSpace = 0;
 	let filesDefsDiff = 0;
 
 	// adjust side panels' height on scroll
@@ -75,8 +160,8 @@
 
 		let filesTop = Math.max(0, filesEl.getBoundingClientRect().top);
 		let scrollTopWithFooter = document.body.scrollTop + document.body.clientHeight + footerHeight + margin;
-		let footerSizeFiles = Math.max(0, scrollTopWithFooter - document.body.scrollHeight);
-		let footerSizeDefs = Math.max(0, scrollTopWithFooter + filesDefsDiff - document.body.scrollHeight);
+		let footerSizeFiles = Math.max(bottomSpace, scrollTopWithFooter - document.body.scrollHeight);
+		let footerSizeDefs = Math.max(bottomSpace, scrollTopWithFooter + filesDefsDiff - document.body.scrollHeight);
 
 		let defsExtra = filesDefsDiff;
 		if (document.body.scrollHeight < scrollTopWithFooter + filesDefsDiff) {
@@ -125,7 +210,7 @@
 		el.classList.add('highlight');
 		setTimeout(() => {
 			el.classList.remove('highlight');
-		}, 600);
+		}, 700);
 	}
 
 	$: unpack(docsData);
@@ -159,12 +244,12 @@
 			<div class="definitions" style:max-height={defsPanelHeight} style:top={defsPanelTop} bind:this={defsEl}>
 				{#each definitions as definition}
 					<a
-						class="definition"
+						class="definition nesting-{getModuleNesting(definition.id)}"
 						class:in-viewport={false}
-						href="#{definition}"
+						href="#{definition.id}"
 						on:click={definitionOnClick}
 					>
-						{definition}
+						<span class="def-kind def-kind-{definition.kind}">{definition.kind.split('-')[0]}</span> <span class="def-name">{definition.name}</span>
 					</a>
 				{/each}
 			</div>
@@ -238,12 +323,6 @@
 		border-radius: 5px;
 	}
 
-	.files {
-		padding-right: 2px;
-		background: white;
-		z-index: 2;
-	}
-
 	.file, .definition {
 		display: block;
 		padding: 6px 12px;
@@ -253,6 +332,23 @@
 		color: var(--color-primary);
 	}
 
+	/* files */
+	.files {
+		padding-right: 2px;
+		background: white;
+		z-index: 2;
+	}
+
+	.file:hover {
+		background: rgb(247, 247, 247);
+	}
+
+	.file.selected {
+		background: var(--color-secondary);
+		color: black;
+	}
+
+	/* definitions */
 	.definitions {
 		border-left: 2px solid rgb(246, 246, 246);
 		padding-left: 2px;
@@ -262,12 +358,42 @@
 		font-weight: normal;
 	}
 
-	.file:hover {
-		background: rgb(247, 247, 247);
+	.definition:hover .def-name {
+		filter: brightness(1.3) contrast(1.3);
+	}
+	.definition:hover .def-kind-func + .def-name {
+		filter: brightness(1.2) contrast(1.25);
 	}
 
-	.file.selected {
-		background: var(--color-secondary);
+	.definition.nesting-1 { margin-left: 20px; }
+	.definition.nesting-2 { margin-left: 40px; }
+
+	.def-kind {
+		color: #9f9f9f;
+		opacity: 1;
+	}
+
+	.def-kind-module + .def-name,
+	.def-kind-type-actor + .def-name,
+	.def-kind-type + .def-name {
+		color: hsl(213 60% 40% / 1);
+	}
+	.def-kind-type-actor + .def-name {
+		font-weight: 500;
+	}
+	.def-kind-class + .def-name {
+		color: hsl(0deg 50% 45%);
+	}
+	.def-kind-module + .def-name {
+		font-weight: 700;
+	}
+	.def-kind-class + .def-name {
+		font-weight: 500;
+	}
+	.def-kind-func + .def-name {
+		color: hsl(35.08deg 51.75% 44.71%);
+	}
+	.def-kind-value + .def-name {
 		color: black;
 	}
 
@@ -281,8 +407,13 @@
 		min-height: 1500px;
 	}
 
+	:global(h2) {
+		margin-top: 1em;
+		margin-bottom: 0.7em;
+	}
+
 	:global(h2, h3) {
-		transition: background-color cubic-bezier(0, 0.79, 0.56, 0.5) 0.6s;
+		transition: background-color cubic-bezier(0, 0.79, 0.76, 0.5) 700ms;
 	}
 
 	:global(h2.highlight, h3.highlight) {
@@ -303,4 +434,8 @@
 		background: rgb(246 248 248);
 		line-height: 1.5;
 	}
+
+	/* :global(#preamble .paragraph:first-child p:first-child) {
+		margin-top: 0.5em;
+	} */
 </style>
