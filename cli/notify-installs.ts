1ca922c196eb1760f67140da7549f6250e7b30ac
Zen Voich <zen.voich@gmail.com>
2023-10-04 12:44:42 +0400

    fix notifyInstalls

--- cli/notify-installs.ts
+++ cli/notify-installs.ts
@@ -2,8 +2,10 @@ import {mainActor} from './mops.js';
 import {resolvePackages} from './resolve-packages.js';
 
 export async function notifyInstalls(names: string[]) {
-	let actor = await mainActor();
 	let resolvedPackages = await resolvePackages();
 	let packages: [string, string][] = names.map(name => [name, resolvedPackages[name] as string]);
-	actor.notifyInstalls(packages);
+	if (packages.length) {
+		let actor = await mainActor();
+		await actor.notifyInstalls(packages);
+	}
 }
