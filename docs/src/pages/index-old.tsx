bdfd24a9a16728cfa0de41620378cf3ffc0e3514
Zen Voich <zen.voich@gmail.com>
2023-10-26 16:38:46 +0400

    -

--- docs/src/pages/index-old.tsx
+++ docs/src/pages/index-old.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/cli">
+            to="/">
             Documentation
           </Link>
         </div>
