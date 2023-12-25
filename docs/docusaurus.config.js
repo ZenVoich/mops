b1c32d7196007dc4ecd88fee2c7d615bb9825d68
Zen Voich <zen.voich@gmail.com>
2023-12-25 12:34:38 +0400

    [docs] update social card image

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -63,11 +63,11 @@ const config = {
 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 		({
 			// Replace with your project's social card
-			image: 'img/docusaurus-social-card.jpg',
+			image: 'img/logo.svg',
 			navbar: {
 				title: 'Documentation',
 				logo: {
-					alt: 'My Site Logo',
+					alt: 'Mops logo',
 					src: 'img/logo.svg',
 				},
 				items: [
