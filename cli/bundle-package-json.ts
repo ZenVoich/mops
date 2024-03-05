30ec6d06250f857d8fdb84dd75e0d0f1cf15fa69
Zen Voich <zen.voich@gmail.com>
2024-03-05 16:49:22 +0400

    [cli] fix release script

--- cli/bundle-package-json.ts
+++ cli/bundle-package-json.ts
@@ -4,10 +4,10 @@ let packageJson = JSON.parse(readFileSync('./bundle/package.json', 'utf8'));
 
 delete packageJson.scripts;
 delete packageJson.devDependencies;
-delete packageJson.overrides
-;
+delete packageJson.overrides;
 packageJson.dependencies = {
 	'decomp-tarxz': packageJson.dependencies['decomp-tarxz'],
+	'buffer': packageJson.dependencies['buffer'],
 };
 
 writeFileSync('./bundle/package.json', JSON.stringify(packageJson, null, '  '));
