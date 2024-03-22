c18ef3126e1a99ca613480fa0694f659967548b2
Zen Voich <zen.voich@gmail.com>
2024-03-22 09:00:39 +0400

    -

--- frontend/components/ColorizedValue.svelte
+++ frontend/components/ColorizedValue.svelte
@@ -2,7 +2,6 @@
 	export let value : number;
 	let color = +value.toFixed(2) < 0 ? 'green' : value > 0 ? 'red' : 'gray';
 	let sign = value > 0 ? '+' : '';
-
 </script>
 
 <span style="color: {color}">{sign}{+value.toFixed(2)}%</span>
