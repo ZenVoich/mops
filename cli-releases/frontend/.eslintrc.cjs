6cd8e0c421febc5f9f29ce36281eb1b15b432326
Zen Voich <zen.voich@gmail.com>
2024-03-26 11:06:25 +0400

    fix lint

--- cli-releases/frontend/.eslintrc.cjs
+++ cli-releases/frontend/.eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
 	extends: [
 		'eslint:recommended',
 		// 'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
+		'plugin:svelte/base',
 	],
 	// parser: '@typescript-eslint/parser',
 	// plugins: ['@typescript-eslint'],
