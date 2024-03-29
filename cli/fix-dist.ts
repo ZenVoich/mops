9534f416e0e77b92010ce600c7516a9b1f63bbd0
Zen Voich <zen.voich@gmail.com>
2024-03-29 11:53:22 +0400

    [cli] fix bin path for local cli build

--- cli/fix-dist.ts
+++ cli/fix-dist.ts
@@ -1,6 +1,12 @@
 import {readFileSync, writeFileSync} from 'node:fs';
 
+// remove scripts
 let text = readFileSync('dist/package.json', 'utf8');
 let json = JSON.parse(text);
 delete json.scripts;
+
+// dist/bin/cli.js -> bin/cli.js
+json.bin.mops = 'bin/mops.js';
+json.bin['ic-mops'] = 'bin/mops.js';
+
 writeFileSync('dist/package.json', JSON.stringify(json, null, 2));
