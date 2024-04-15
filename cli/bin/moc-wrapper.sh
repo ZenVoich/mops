0e0a4d255c901ca874185ff54fb46c0b7d0dfa23
Zen Voich <zen.voich@gmail.com>
2024-04-15 13:04:56 +0400

    [cli] optimize `moc-wrapper`

--- cli/bin/moc-wrapper.sh
+++ cli/bin/moc-wrapper.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
-mocPath="$(mops toolchain bin moc --fallback)"
+
+# TODO: find nearest mops.toml if we are inside subfolders
+rootDir="$(pwd)"
+mopsToml="$rootDir/mops.toml"
+
+if [ ! -f $mopsToml ]; then
+  echo "$mopsToml not found"
+  exit 1;
+fi;
+
+mopsTomlHash=$(openssl sha256 $mopsToml | awk -F'= ' '{print $2}')
+cached="$rootDir/.mops/moc-$mopsTomlHash"
+
+if [ -f $cached ]; then
+  mocPath=$(cat $cached)
+  if [[ "$mocPath" != *"/moc" ]] ; then
+    mocPath="$(mops toolchain bin moc --fallback)"
+    echo -n $mocPath > "$cached"
+  fi;
+else
+  mocPath="$(mops toolchain bin moc --fallback)"
+  echo -n $mocPath > "$cached"
+fi;
+
 $mocPath "$@"
