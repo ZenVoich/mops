981660894a35303896e7757763de2beac66d6bb0
Zen Voich <zen.voich@gmail.com>
2024-04-24 11:55:54 +0400

    [cli] fix: fallback to dfx moc if there is no mops.toml

--- cli/bin/moc-wrapper.sh
+++ cli/bin/moc-wrapper.sh
@@ -14,27 +14,26 @@ rootDir=$(findRootDir)
 mopsToml="$rootDir/mops.toml"
 
 if [[ $rootDir == "" ]] || [[ ! -f $mopsToml ]]; then
-  echo "mops.toml not found in $rootDir or its parent directories"
-  exit 1;
-fi;
-
-if command -v openssl &> /dev/null; then
-  mopsTomlHash=$(openssl sha256 $mopsToml | awk -F'= ' '{print $2}')
+  mocPath="$(mops toolchain bin moc --fallback)"
 else
-  mopsTomlHash=$(shasum $mopsToml -a 256 | awk -F' ' '{print $1}')
-fi;
+  if command -v openssl &> /dev/null; then
+    mopsTomlHash=$(openssl sha256 $mopsToml | awk -F'= ' '{print $2}')
+  else
+    mopsTomlHash=$(shasum $mopsToml -a 256 | awk -F' ' '{print $1}')
+  fi;
 
-cached="$rootDir/.mops/moc-$mopsTomlHash"
+  cached="$rootDir/.mops/moc-$mopsTomlHash"
 
-if [ -f $cached ]; then
-  mocPath=$(cat $cached)
-  if [[ "$mocPath" != *"/moc" ]] ; then
+  if [ -f $cached ]; then
+    mocPath=$(cat $cached)
+    if [[ "$mocPath" != *"/moc" ]] ; then
+      mocPath="$(mops toolchain bin moc --fallback)"
+      echo -n $mocPath > "$cached"
+    fi;
+  else
     mocPath="$(mops toolchain bin moc --fallback)"
     echo -n $mocPath > "$cached"
   fi;
-else
-  mocPath="$(mops toolchain bin moc --fallback)"
-  echo -n $mocPath > "$cached"
 fi;
 
 $mocPath "$@"
