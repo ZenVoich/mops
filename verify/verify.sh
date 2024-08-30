9320d6ed79802a2ae308e1df48a750efab9eacef
Zen Voich <zen.voich@gmail.com>
2024-08-30 17:20:34 +0400

    [cli] verify build WIP

---
+++ verify/verify.sh
@@ -0,0 +1,24 @@
+# COMMIT_HASH=b18cafb288b25d48caa922ea69216a12a511ff25
+# MOPS_VERSION=1.0.0-pre.0
+# SHASUM=1c78eab58e41282a071218169062dc4f82ac7ab6ae45f95630150ae2f2fca0e2
+
+# git clone https://github.com/ZenVoich/mops.git
+# cd mops
+# git checkout $COMMIT_HASH
+
+# cd cli
+# npm install
+# npm version $MOPS_VERSION
+# npm run release
+
+# actual_shasum=$(shasum -a 256 mops/cli/bundle/cli.tgz | head -n1 | awk '{print $1;}')
+actual_shasum=$(sha256sum bundle/cli.tgz | head -n1 | awk '{print $1;}')
+
+if [ "$actual_shasum" != "$SHASUM" ]; then
+  echo "Expected shasum: $SHASUM"
+  echo "Actual shasum: $actual_shasum"
+  echo "Verification failed"
+  exit 1
+else
+  echo "Verification successful for shasum: $actual_shasum"
+fi
