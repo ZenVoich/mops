4c22b5ec37c5da9c8231656cf56f8a06dba09308
Zen Voich <zen.voich@gmail.com>
2024-09-09 17:11:06 +0400

    -

--- cli/DEVELOPMENT.md
+++ cli/DEVELOPMENT.md
@@ -11,7 +11,22 @@
 npm publish
 ```
 
-## Publish on chain
+## Publish on chain verifiable build
+
+1. Push latest commit to `main` branch
+
+1. Build verifiable build using Docker
+
+```
+cd verify
+cid=$(docker create mops)
+docker build . --build-arg COMMIT_HASH=<commit_hash> --build-arg MOPS_VERSION=<mops_version> -t mops
+```
+
+2. Copy `cli.tgz` to the root of the project
+```
+docker run --rm --env SHASUM=<sha256sum> mops
+```
 
 1. Prepeare release
 ```
--- verify/verify.sh
+++ verify/verify.sh
@@ -23,12 +23,12 @@ else
   echo "Verification successful for shasum: $actual_shasum"
 fi
 
-# build and copy cli.tgz
+# build:
+# docker build . --build-arg COMMIT_HASH=1f69fb4076cef8f4f27b7422a1ac0c184fa37126 --build-arg MOPS_VERSION=0.0.0 -t mops
+
+# copy cli.tgz:
 # cid=$(docker create mops)
 # docker cp $cid:/mops/cli/bundle/cli.tgz .
-
-# build:
-# docker build . --build-arg COMMIT_HASH=b18cafb288b25d48caa922ea69216a12a511ff25 --build-arg MOPS_VERSION=0.5.1 -t mops
 
 # verify:
 # docker run --rm --env SHASUM=d57fc281a24fb12b0b6ef72cac4d478b357e6f22dca53bcc4a5506759fc96b06 mops
