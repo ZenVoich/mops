e0ed99542f461febc8e11722c381aefba70a982d
Zen Voich <zen.voich@gmail.com>
2024-09-11 11:23:35 +0400

    -

--- cli/verify.sh
+++ cli/verify.sh
@@ -8,13 +8,3 @@ if [ "$actual_shasum" != "$SHASUM" ]; then
 else
   echo "Verification successful for shasum: $actual_shasum"
 fi
-
-# build:
-# docker build . --build-arg COMMIT_HASH=1f69fb4076cef8f4f27b7422a1ac0c184fa37126 --build-arg MOPS_VERSION=0.0.0 -t mops
-
-# copy cli.tgz:
-# cid=$(docker create mops)
-# docker cp $cid:/mops/cli/bundle/cli.tgz .
-
-# verify:
-# docker run --rm --env SHASUM=d57fc281a24fb12b0b6ef72cac4d478b357e6f22dca53bcc4a5506759fc96b06 mops
