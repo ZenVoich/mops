aa6b146e2d85f487b56b00b26386802f19f8b0f8
Zen Voich <zen.voich@gmail.com>
2024-11-25 13:15:37 +0400

    [frontend] show package version publisher

--- frontend/components/package/UserCard.svelte
+++ frontend/components/package/UserCard.svelte
@@ -5,27 +5,30 @@
 	import {User} from '/declarations/main/main.did';
 
 	export let user : User;
+	export let compact = false;
 </script>
 
-<div class="user-card">
+<div class="user-card" class:compact={compact}>
 	<div class="value user">
 		{#if user.name}
-			<a class="value" href="/search/owner:{user.name}" use:link>@{user.name}</a> <small>{user.id}</small>
+			<a class="value" href="/search/owner:{user.name}" use:link>{user.name}</a> <small class="principal-text">{user.id}</small>
 		{:else}
 			<a class="value" href="/search/owner:{user.id}" use:link>{user.id}</a>
 		{/if}
 	</div>
-	{#if user.github}
-		<a class="value with-icon" href="https://github.com/{user.github}" target="_blank">
-			<img class="github-icon" src="{githubImg}" alt="GitHub logo" loading="lazy" />
-			{user.github}
-		</a>
-	{/if}
-	{#if user.twitter}
-		<a class="value with-icon" href="https://twitter.com/{user.twitter}" target="_blank">
-			<img class="twitter-icon" src="{twitterImg}" alt="Twitter logo" loading="lazy" />
-			{user.twitter}
-		</a>
+	{#if !compact}
+		{#if user.github}
+			<a class="value with-icon" href="https://github.com/{user.github}" target="_blank">
+				<img class="github-icon" src="{githubImg}" alt="GitHub logo" loading="lazy" />
+				{user.github}
+			</a>
+		{/if}
+		{#if user.twitter}
+			<a class="value with-icon" href="https://twitter.com/{user.twitter}" target="_blank">
+				<img class="twitter-icon" src="{twitterImg}" alt="Twitter logo" loading="lazy" />
+				{user.twitter}
+			</a>
+		{/if}
 	{/if}
 </div>
 
@@ -49,7 +52,15 @@
 		filter: hue-rotate(45deg) contrast(0.6);
 	}
 
-	.user-card {
+	.user-card.compact .principal-text::before {
+		content: '(';
+	}
+
+	.user-card.compact .principal-text::after {
+		content: ')';
+	}
+
+	.user-card:not(.compact) {
 		display: flex;
 		flex-direction: column;
 		gap: 6px;
